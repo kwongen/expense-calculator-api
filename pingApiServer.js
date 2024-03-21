787270d4533d5775cbb5b55c6ed7d656f9f7a73b
kwongen <153678379+kwongen@users.noreply.github.com>
2024-03-21 10:58:03 +0000

    update ping freq

--- pingApiServer.js
+++ pingApiServer.js
@@ -2,16 +2,14 @@ const pingApiServer = async () => {
 
   const ping = async () => {
     console.log("pinging the api server at:", new Date());
-    const resp = await fetch("https://api.expensecalculator.co.uk/api/auth/refresh-token", {
+    await fetch("https://api.expensecalculator.co.uk/api/auth/refresh-token", {
         signal: AbortSignal.timeout(200000),
       });
     console.log("finish at:", new Date());
-    // const result = await resp.text();
-    // console.log("result:", result)
   }
 
-  ping();
-  setInterval(() => ping(), 60000)
+  ping(); // perform first ping
+  setInterval(() => ping(), 840000) // ping in every 14 min 
 }
 
 module.exports = {
